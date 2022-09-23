d679078dea27d8bab6adb1f039fd259d114b30b9
Artem Plyasulya <80699430+hamelons@users.noreply.github.com>
2022-09-23 19:34:34 +0300

    Delete src directory

--- src/js/loader.js
+++
@@ -1,9 +0,0 @@
-document.body.onload = function() {
-
-    setTimeout(function() {
-        let preloader = document.getElementById('page__preloader');
-        if (!preloader.classList.contains('active')) {
-            preloader.classList.add('active');
-        }
-    }, 2000);
-}
--- src/js/scrollTop.js
+++
@@ -1,9 +0,0 @@
-const scrollTop = document.querySelector('.scroll__top');
-
-window.addEventListener('scroll', () => {
-    if(window.pageYOffset > 1600) {
-        scrollTop.classList.add('active');
-    } else {
-        scrollTop.classList.remove('active');
-    }
-});
